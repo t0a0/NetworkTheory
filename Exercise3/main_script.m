a98ce8af3c444293e1d54ab7ec83a71b28818e5b
sVujke <stefanvujovic93@gmail.com>
2017-06-11 18:07:17 +0200

    task2

--- Exercise3/main_script.m
+++ Exercise3/main_script.m
@@ -3,19 +3,47 @@
 1;
 clear;
 
-function [A] = load_sparse_as_identical(filename)
-C = load(filename);
-i = C'(1,:);
-j = C'(2,:);
-sv(1:length(i)) = 1;
-dimension = max(max(i),max(j));
-A = full(sparse(i,j,sv,dimension,dimension));
+function [A] = load_sparse(filename,range)
+
+if(size(range)(1) == 0)
+  E = load(filename);
+else
+  E = dlmread(filename,' ',range);
+endif
+n = max(max(E));
+A = sparse(E(:,1), E(:,2), 1, n, n);
 A = A + A';
 endfunction
-B = load_sparse_as_identical("prediction-graph.txt");
+
+empty_range = [];
+B = load_sparse("prediction-graph.txt",empty_range);
 
 [U, Lambda] = eig(B);
 
-d = diag(Lambda);
-max_eigen_value = max(d)
-min_eigen_value = min(d)
+max_eigen_value = max(Lambda);
+min_eigen_value = min(Lambda);
+
+fb = "facebook-wosn-links/out.facebook-wosn-links";
+
+C = load_sparse(fb,[2, 0, 817037, 1]);
+x= eigs(C, 10);
+vec_eigs = x/max(x);
+
+function [dom, vec] = power_iter(A) 
+dim = size(A)(1);
+vec = ones(dim,1);
+dom = 0;
+threshold = 10;
+do
+  vec_old = vec;
+  vec = A*vec;
+  dom = max(vec);
+  vec = vec/dom;
+until (norm(vec-vec_old) < threshold && abs(vec - vec_old)<threshold)
+endfunction
+
+[dom,vec] = power_iter(C);
+
+vec_power_iter = sort(vec, "descend")(1:10);
+diff = norm(vec_power_iter - vec_eigs);
+
